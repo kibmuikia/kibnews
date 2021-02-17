b7fb14934e4674775a68914260016be4de17c394
kibdev <dev_at_labs.sporopollenin@simplelogin.fr>
2021-02-17 10:14:15 +0300

    lesson: using ViewModel

--- app/src/main/java/kib/dev/kibnews/ui/home/HomeFragment.kt
+++ app/src/main/java/kib/dev/kibnews/ui/home/HomeFragment.kt
@@ -7,25 +7,61 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import kib.dev.kibnews.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
+    private val logTag: String = HomeFragment::class.java.simpleName
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var rootView: View
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeViewModel()
+        initializeListeners()
+    }
+
+    override fun onDestroyView() {
+        //rootView = null
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    private fun observeViewModel() {
+        //.
+
+        val textView: TextView = rootView.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
-        return root
+
+        homeViewModel.badgeCount.observe(viewLifecycleOwner, Observer {
+            text_home?.text = it.toString()
+        })
+    }
+
+    private fun initializeListeners() {
+
+        btn_frag_home_badgecount?.setOnClickListener {
+            homeViewModel.incrementBadgeCount()
+        }
     }
 }
--- app/src/main/java/kib/dev/kibnews/ui/home/HomeViewModel.kt
+++ app/src/main/java/kib/dev/kibnews/ui/home/HomeViewModel.kt
@@ -1,10 +1,31 @@
 package kib.dev.kibnews.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
+
+    // **
+    val logTag = HomeViewModel::class.java.simpleName
+
+    private val _badgeCount = MutableLiveData<Int>()
+    var number = 0
+
+    val badgeCount: LiveData<Int>
+        get() = _badgeCount
+
+    fun incrementBadgeCount() {
+        _badgeCount.postValue(++number)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.e(logTag, ": onCleared: init")
+        //number = 0
+    }
+    // **
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
