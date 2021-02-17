cccdeed66cc37ca6bb26ef44f96a58108740006d
kibdev <dev_at_labs.sporopollenin@simplelogin.fr>
2021-02-17 14:01:59 +0300

    initial New-Api constants

---
+++ app/src/main/java/kib/dev/kibnews/tools/BasicConstants.kt
@@ -0,0 +1,8 @@
+package kib.dev.kibnews.tools
+
+// News Api Constants
+const val NEWSAPI_BASE_URL = "http://newsapi.org/"
+const val NEWSAPI_VERSION = "v2"
+const val NEWSAPI_EP_TOPHEADLINES = "/top-headlines"
+const val NEWSAPI_EP_EVERYTHING = "/everything"
+const val NEWSAPI_EP_SOURCES = "/sources"
