15e79d1c0747fdc12e68484bcc0cc6cd88b6adea
kibdev <dev_at_labs.sporopollenin@simplelogin.fr>
2021-02-18 09:47:22 +0300

    renamed to SearchFragment.kt and SettingsFragment.kt

--- app/src/main/java/kib/dev/kibnews/ui/activities/main_activity/fragments/settings/SettingsViewModel.kt
+++ app/src/main/java/kib/dev/kibnews/ui/activities/main_activity/fragments/settings/SettingsViewModel.kt
@@ -1,13 +1,13 @@
-package kib.dev.kibnews.ui.activities.main_activity.fragments.notifications
+package kib.dev.kibnews.ui.activities.main_activity.fragments.settings
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class SettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is Settings Fragment"
     }
     val text: LiveData<String> = _text
 }
